07700ccc3d5e7068a0f4e4f8ec83e7381415c881
Egor Kloos <egor+git@lunatech.com>
2011-11-18 15:17:51 +0100

    RequireJS (lazy loading external scripts)

--- scripts/init.js
+++ scripts/init.js
@@ -4,6 +4,83 @@
     
     $(document).ready(function() {
     
+        //  jQuery UI datapicker (lazyloaded)
+        $( "input[name*='date'],input[name*='Date']" ).each(function(){ 
+            var $elem = $(this);
+            require(["jquery-ui"], function(){
+                $elem.datepicker({ dateFormat: 'dd-mm-yy' }); 
+            });
+        });
+        
+        //  jQuery UI button loader (lazyloaded)
+        $('body').bind('buttonLoader', function(){
+            $(".button,input[type='submit'],button,input[type='button']").each(function(){
+                var $elem = $(this);
+                require(["jquery-ui"], function(){
+                    
+                    $elem.button();
+
+                    // ADDING ICONS TO BUTTONS
+                    if($elem.is(".open")){$elem.button().click(function(){$(this).closest('.quick-add').find('.objectForm').toggle('slow');});}
+                    if($elem.is(".document,.report")){$elem.button({icons:{primary:"ui-icon-document"}});}
+                    if($elem.is(".add")){$elem.button({icons:{primary:"ui-icon-plus"}});}
+                    if($elem.is(".edit")){$elem.button({icons:{primary:"ui-icon-pencil"}});}
+                    if($elem.is(".save")){$elem.button({icons:{primary:"ui-icon-disk"}});}
+                    if($elem.is(".save.edit")){$elem.button({icons:{primary:"ui-icon-disk"}},{icons:{secondary:"ui-icon-pencil"}});}
+                    if($elem.is(".save.add")){$elem.button({icons:{primary:"ui-icon-disk"}},{icons:{secondary:"ui-icon-plus"}});}
+                    if($elem.is(".delete")){$elem.button({icons:{primary:"ui-icon-trash"}});}
+                    if($elem.is(".copy")){$elem.button({icons:{primary:"ui-icon-copy"}});}
+                    if($elem.is(".subscribe, .unsubscribe")){$elem.button({icons:{primary:"ui-icon-star"}});}
+                    if($elem.is("button.comment")){$elem.button({icons:{primary:"ui-icon-comment"}});}
+                    if($elem.is(".import")){$elem.button({icons:{primary:"ui-icon-arrowthickstop-1-s"}});}
+                    if($elem.is(".restore")){$elem.button({icons:{primary:"ui-icon-arrowthickstop-1-s"}},{icons:{secondary:"ui-icon-folder-open"}});}
+                    if($elem.is(".backup")){$elem.button({icons:{primary:"ui-icon-arrowthickstop-1-n"}},{icons:{secondary:"ui-icon-folder-collapsed"}});}
+                    if($elem.is(".print")){$elem.button({icons:{primary:"ui-icon-print"}}).click(function(){ window.print() });}
+                    if($elem.is(".download")){$elem.button({icons:{primary:"ui-icon-circle-arrow-s"}});}
+                    if($elem.is(".done, .checked")){$elem.button({icons:{primary:"ui-icon-check"}});}
+                    if($elem.is(".close")){$elem.button({icons:{primary:"ui-icon-closethick"}});}
+                    if($elem.is(".cancel,.clear")){$elem.button({icons:{primary:"ui-icon-cancel"}});}
+                    if($elem.is(".invite")){$elem.button({icons:{primary:"ui-icon-mail-open"}});}
+                    if($elem.is(".search")){$elem.button({icons:{primary:"ui-icon-search"}});}
+                    if($elem.is(".timer")){$elem.button({icons:{primary:"ui-icon-clock"}});}
+                    if($elem.is(".signin")){$elem.button({icons:{primary:"ui-icon-locked"}});}
+                });
+            });
+    
+        });
+
+        //  Accordion (lazyloaded)
+        $("#accordion").each(function(){
+            var $elem = $(this);
+            require(["jquery-ui"], function(){
+                $elem.accordion({ header: "h3", autoHeight: false });
+            });
+        });
+        
+        //  Tabs  (lazyloaded)
+        $('.tabs').each(function(){
+            var $elem = $(this);
+            require(["jquery-ui"], function(){
+                $elem.tabs();
+            });
+        });
+        
+        //  DATATABLE (lazyloaded)
+        $(".datatable").each(function(){
+            var $elem = $(this);
+            require(["datatables"], function(){
+                $elem.dataTable( {
+                    "bJQueryUI": true,
+                    "aLengthMenu": [[5, 10, 20, -1], [5, 10, 20, "All"]],
+                    "iDisplayLength": 5,
+                    "bLengthChange": true,
+                    "sPaginationType": "full_numbers",
+                    "aoColumns": [ null, null, null, { "bSortable": false } ]
+                });
+            });
+        });
+        
+        
         // LOAD FLASH OBJECT (SWF FILE)
         $("object > param[value$='swf']").each(function(index){
             var f = $(this).closest('object').attr('id');
@@ -11,21 +88,9 @@
             if (index>0) { regOb(f); } else { $.getScript("scripts/swfobject/swfobject.js",function(){ regOb(f); }); }
         });
 
-        $( "input[name*='date'],input[name*='Date'],.date" ).each(function(){$(this).datepicker({ dateFormat: 'dd-mm-yy' })});
-        $(".button,[type=submit],button,[type=button]").button();
-                
-        // ADDING ICONS TO BUTTONS
-        $(".add").button({icons:{primary:"ui-icon-plus"}});
-        $(".edit").button({icons:{primary:"ui-icon-pencil"}});
-        $(".save").button({icons:{primary:"ui-icon-disk"}});
-        $(".save.edit").button({icons:{primary:"ui-icon-disk"}},{icons:{secondary:"ui-icon-pencil"}});
-        $(".save.add").button({icons:{primary:"ui-icon-disk"}},{icons:{secondary:"ui-icon-plus"}});
-        $(".delete").button({icons:{primary:"ui-icon-trash"}});
-        $(".cancel").button({icons:{primary:"ui-icon-cancel"}});
-        $(".restore").button({icons:{primary:"ui-icon-arrowthickstop-1-s"}},{icons:{secondary:"ui-icon-folder-collapsed"}});
-        $(".backup").button({icons:{primary:"ui-icon-arrowthickstop-1-n"}},{icons:{secondary:"ui-icon-folder-open"}});
-        $(".print").button({icons:{primary:"ui-icon-print"}}).click(function(){ window.print() });
-                
+        // Load JavaScript generated UI elements
+        $('body').each(function(){$(this).trigger('buttonLoader')});
+
         $("tbody").delegate("tr", "hover", function(){
             $('td', this).toggleClass("hover");
         });
@@ -33,28 +98,6 @@
             $("input[placeholder]").removeAttr('placeholder');
         }
         
-        // Accordion
-        $("#accordion").each(function(){
-          $(this).accordion({ header: "h3", autoHeight: false });
-        });
-        
-        // Tabs
-        $('.tabs').each(function(){
-          $(this).tabs();
-        })
-        
-        
-        $(".datatable").each(function(){
-            $(this).dataTable( {
-                "bJQueryUI": true,
-                "aLengthMenu": [[5, 10, 20, -1], [5, 10, 20, "All"]],
-                "iDisplayLength": 5,
-                "bLengthChange": true,
-                "sPaginationType": "full_numbers",
-                "aoColumns": [ null, null, null, { "bSortable": false } ]
-            });
-        });
-    
         $("canvas.dummy").each(function(){
             var el = $(this).get()[0];
             var ctx = el.getContext('2d');
@@ -71,3 +114,5 @@
         });
 
     });    
+    
+    define("init", function(){});
