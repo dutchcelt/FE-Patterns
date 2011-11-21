878466bf5fd85c126fd6b965369e58ba4ac26ebf
Egor Kloos <egor+git@lunatech.com>
2011-11-21 15:13:05 +0100

    Some JSHint cleanup

--- scripts/keepinview.js
+++ scripts/keepinview.js
@@ -18,8 +18,8 @@
                     type: element.css('position'),
                     top: element.css('top'),
                     left: element.css('left')
-                }
-                var marginOffset = (cssPosition.left=="auto") ? parseInt(element.css('marginLeft')) : 0;
+                };
+                var marginOffset = (cssPosition.left==="auto") ? parseInt(element.css('marginLeft'),10) : 0;
                 var h = element.height(),
                     w = element.width();
                 
@@ -30,29 +30,29 @@
                         width:      w,
                         height:     h 
                     });
-                }
+                };
                 var fixCSS = function(t){
                     element.css({ 
                         top:        t+'px'
                     });
-                }
+                };
                 var clearCSS = function(){
                     element.css({ 
                         position:   cssPosition.type,
                         left:       cssPosition.left,
                         top:        cssPosition.top 
                     });
-                }
+                };
                 
                 function setElem(){
                     setCSS();
-                    if (orientation==="bottom" && offset.top !=0) {
-                        if( $(window).height() < parseInt(offset.top + element.outerHeight() - Math.abs($(window).scrollTop())+edgeOffset) ) { 
+                    if (orientation==="bottom" && offset.top !==0) {
+                        if( $(window).height() < parseInt(offset.top + element.outerHeight() - Math.abs($(window).scrollTop())+edgeOffset,10) ) { 
                             fixCSS(($(window).height()-element.outerHeight()-edgeOffset));
                         } else { 
                             clearCSS();
                         }
-                    } else if (orientation==="top" && offset.top !=0) {
+                    } else if (orientation==="top" && offset.top !==0) {
                         if( ($(window).scrollTop())+edgeOffset > offset.top ) { 
                             fixCSS(edgeOffset);
                         } else { 
@@ -65,7 +65,7 @@
                     setElem();
                 }).trigger('scroll');
                 
-            }
+            };
         
         })(jQuery);   
         
