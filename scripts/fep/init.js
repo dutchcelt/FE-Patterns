d7eb8df624920980edf1ed90d21e0f870900ce82
Egor Kloos <egor+git@lunatech.com>
2013-05-26 17:43:25 +0200

    Updated jQuery to version 2.0.1

--- scripts/fep/init.js
+++ scripts/fep/init.js
@@ -9,7 +9,7 @@
     require.config({ 
         baseUrl: "/scripts/minified",
         paths: {
-            "jquery": "jquery-1.9.1-min",
+            "jquery": "jquery-2.0.1.min",
             "config": "config-min",
             "keepinview": "keepinview-min",
             "datatable": "jquery.dataTables-min",
